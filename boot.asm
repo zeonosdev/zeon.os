4f55cb64a6c05e39f09705e4e6c09e199f0672b6
ZeonOS <nullsofty@gmail.com>
2026-07-23 21:40:36 +0700

    boot.asm

---
+++ boot.asm
@@ -0,0 +1,69 @@
+; ============================================
+; ZEONOS v0.3.0 - BOOT.ASM
+; Target Architecture: x86 (IA-32) Bare-Metal
+; Description: Multiboot header & Kernel Entry Point
+; ============================================
+
+[BITS 32]
+
+MULTIBOOT_PAGE_ALIGN    equ 1 << 0
+MULTIBOOT_MEMORY_INFO   equ 1 << 1
+MULTIBOOT_HEADER_MAGIC  equ 0x1BADB002
+MULTIBOOT_HEADER_FLAGS  equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+MULTIBOOT_CHECKSUM      equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+section .multiboot
+align 4
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADER_FLAGS
+    dd MULTIBOOT_CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+    resb 65536
+stack_top:
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    cli
+    mov esp, stack_top
+
+    push 0
+    popf
+
+    push ebx
+    push eax
+
+    call kernel_main
+
+.hang:
+    cli
+    hlt
+    jmp .hang
+
+global gdt_flush
+gdt_flush:
+    mov eax, [esp + 4]
+    lgdt [eax]
+
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    jmp 0x08:.flush_complete
+
+.flush_complete:
+    ret
+
+global idt_load
+idt_load:
+    mov eax, [esp + 4]
+    lidt [eax]
+    ret
