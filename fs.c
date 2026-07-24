954419408a0e76cb641793c0cde8f7b2acedf2d3
ZeonOS <nullsofty@gmail.com>
2026-07-24 22:16:23 +0700

    Update fs.c

--- fs.c
+++ fs.c
@@ -1,4 +1,4 @@
-#include "zeonos_h"
+#include "zeonos.h"
 
 struct FileSystem main_filesystem;
 
@@ -6,16 +6,13 @@ void filesystem_initialize(void) {
     memory_set(&main_filesystem, 0, sizeof(main_filesystem));
     main_filesystem.next_free_sector = 100;
     main_filesystem.is_mounted = 1;
-    char label[] = "ZEONOS_ROOT";
-    for(int i = 0; label[i] != '\0'; i++) main_filesystem.volume_label[i] = label[i];
-    kernel_print("Filesystem: Initialized Successfully\n", COLOR_GREEN);
+    kernel_print("Filesystem: Virtual File System (VFS) Mounted\\n", COLOR_LIGHT_BLUE);
 }
 
 int filesystem_create_file(const char* filename, uint32_t size) {
     for (int i = 0; i < MAX_FILES; i++) {
         if (!main_filesystem.files[i].is_used) {
             main_filesystem.files[i].is_used = 1;
-            main_filesystem.files[i].start_sector = main_filesystem.next_free_sector;
             main_filesystem.files[i].file_size = size;
             int len = 0;
             while(filename[len] != '\0' && len < MAX_FILENAME_LEN - 1) {
@@ -23,22 +20,8 @@ int filesystem_create_file(const char* filename, uint32_t size) {
                 len++;
             }
             main_filesystem.files[i].filename[len] = '\0';
-            main_filesystem.next_free_sector += (size + SECTOR_SIZE - 1) / SECTOR_SIZE;
             return i;
         }
     }
     return -1;
-}
-
-void filesystem_list_files(void) {
-    kernel_print("--- File System Directory ---\n", COLOR_LIGHT_CYAN);
-    for (int i = 0; i < MAX_FILES; i++) {
-        if (main_filesystem.files[i].is_used) {
-            kernel_print("File: ", COLOR_WHITE);
-            kernel_print(main_filesystem.files[i].filename, COLOR_WHITE);
-            kernel_print(" | Size: ", COLOR_WHITE);
-            kernel_print_dec(main_filesystem.files[i].file_size, COLOR_WHITE);
-            kernel_print(" bytes\n", COLOR_WHITE);
-        }
-    }
 }
