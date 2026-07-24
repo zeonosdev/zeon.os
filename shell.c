f83faee4f822a64f11afd14cef04fb8a8cf36ebc
ZeonOS <nullsofty@gmail.com>
2026-07-24 21:46:36 +0700

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,7 +1,19 @@
-#include "zeonos_h"
+/*
+ * ZEONOS v0.3.0 - Interactive Shell & Auth Integration
+ * License: GPLv3
+ */
+
+#include "zeonos.h"
 
 void shell_start(void) {
-    kernel_print("ZEONOS Shell v0.1 Ready.\n", COLOR_LIGHT_BROWN);
-    kernel_print("Type 'help' for available commands.\n", COLOR_WHITE);
+    // Inisialisasi Database Auth File
+    auth_init();
+
+    // Jalankan Alur Register -> Simpan DB -> Login
+    auth_prompt_register();
+    auth_prompt_login();
+
+    kernel_print("ZEONOS Interactive Shell v0.1 Ready.\n", COLOR_LIGHT_BROWN);
+    kernel_print("Type 'help' for available commands.\n\n", COLOR_WHITE);
     kernel_print("zeonos# ", COLOR_LIGHT_GREEN);
 }
